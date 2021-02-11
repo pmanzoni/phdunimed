93bec32f74b30913d8aefb3f099cd0f609db2423
pmanzoni <manzoni@ieee.org>
2021-02-11 11:57:15 +0100

    Basic version

---
+++ code/sipub.py
@@ -0,0 +1,48 @@
+# File: sipub.py
+#
+# The simplest MQTT producer.
+
+import random
+import time
+
+import paho.mqtt.client as mqtt
+
+THE_BROKER = "test.mosquitto.org"
+THE_TOPIC = "PMtest/rndvalue"
+CLIENT_ID = ""
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected to ", client._host, "port: ", client._port)
+    print("Flags: ", flags, "returned code: ", rc)
+
+# The callback for when a message is published.
+def on_publish(client, userdata, mid):
+    print("sipub: msg published (mid={})".format(mid))
+
+
+client = mqtt.Client(client_id=CLIENT_ID, 
+                     clean_session=True, 
+                     userdata=None, 
+                     protocol=mqtt.MQTTv311, 
+                     transport="tcp")
+
+client.on_connect = on_connect
+client.on_publish = on_publish
+
+client.username_pw_set(None, password=None)
+client.connect(THE_BROKER, port=1883, keepalive=60)
+
+client.loop_start()
+
+while True:
+
+    msg_to_be_sent = random.randint(0, 100)
+    client.publish(THE_TOPIC, 
+                   payload=msg_to_be_sent, 
+                   qos=0, 
+                   retain=False)
+
+    time.sleep(5)
+
+client.loop_stop()
